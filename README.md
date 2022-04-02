45bb976c8a886ee9ec7d8dc25c102e65108163f7
LfakirAdil <kapobest@gmail.com>
2022-04-02 17:48:24 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Suivi des tâches
+
+Ce projet a été généré avec [Angular CLI] version 13.2.6.
+
+## Development server
+
+Executer `ng serve` pour un serveur de développement. Aller vers `http://localhost:4200/`. 
+L'application se rechargera automatiquement si vous modifiez l'un des fichiers source.
+
+## Base de donnes 
+
+J'ai pas utiliser une base de donnes pour ce projet mais j'ai utiliser un Rest Api fictif JSON-SERVER aller dans package.json pour les script il est executer par defaut PORT 3000 si non vous pouvez ajouter " --port [numero de port souhaiter]"
+
+
---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit ,Input,Output,EventEmitter} from '@angular/core';
+import { Task } from '../../Task'
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+  @Input() task: Task | undefined;
+  @Output() onDeleteTask : EventEmitter<Task> = new EventEmitter()
+  @Output() onToggleReminder : EventEmitter<Task> = new EventEmitter()
+  faTimes = faTimes;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onDelete(task:Task|any) {
+    //console.log("depuis task-iem",task);
+    this.onDeleteTask.emit(task)
+  }
+  onToggle(task:Task|any) {
+    this.onToggleReminder.emit(task)
+  }
+}
